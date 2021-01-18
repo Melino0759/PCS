6375e861904c9d6e3982afeaed732074c68f3035
Melino <melino.30@students.amikom.ac.id>
2021-01-18 15:42:04 +0700

    UAS

---
+++ app/src/main/java/id/Melino/githubuser/data/model/User.kt
@@ -0,0 +1,6 @@
+package id.Melino.githubuser.data.model
+
+data class User(
+    val login: String,
+    val avatar_url: String
+)
---
+++ app/src/main/java/id/Melino/githubuser/data/model/UserResponse.kt
@@ -0,0 +1,5 @@
+package id.Melino.githubuser.data.model
+
+data class UserResponse(
+    val items : ArrayList<User>
+)
